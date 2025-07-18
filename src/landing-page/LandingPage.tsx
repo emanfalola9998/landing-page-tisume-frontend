be55770b2890ced6275c4b36c0e353ce233cde58
emmanuelfalola <emmanuelfalola9998u@gmail.com>
2025-07-18 14:58:22 +0100

    removed used react import

--- src/landing-page/LandingPage.tsx
+++ src/landing-page/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, type ChangeEvent, type FormEvent } from 'react'
+import { useState, type ChangeEvent, type FormEvent } from 'react'
 import './LandingPage.scss'
 
 const LandingPage = () => {
